96b6416949c4f5e719ccba05b2183dc0752f9ccc
AragonParagon <112719672+AragonParagon@users.noreply.github.com>
2023-01-17 10:35:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+
+using SplitPeWebAPI.DB;
+
+
+namespace SplitPeWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<SplitPeContext>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
