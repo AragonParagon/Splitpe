0303e949cf3fe1a4a9b6f967322472d5243e1a60
AragonParagon <112719672+AragonParagon@users.noreply.github.com>
2023-01-17 10:40:08 +0530

    Add files via upload

---
+++ DB/SplitPeContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SplitPeWebAPI.Models;
+namespace SplitPeWebAPI.DB
+{
+    public class SplitPeContext: DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public SplitPeContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("db"));
+            //optionsBuilder.UseMySQL("server=127.0.0.1;port=3306;user=root;password=hypervenomctr360;database=splitpe");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OweTo>().HasNoKey().ToView("Dummy");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<PersonalExpense> PersonalExpenses { get; set; }
+        public DbSet<SplitExpense> SplitExpenses { get; set; }
+
+        public DbSet<Due> Dues { get; set; }
+
+        public DbSet<OweTo> OweTos { get; set; }
+        public DbSet<OweBy> OweBys { get; set; }
+
+    }
+}
